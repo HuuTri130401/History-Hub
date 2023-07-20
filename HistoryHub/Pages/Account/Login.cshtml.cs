36d7b9272a6551a584ca737b8cfdb2fec63c2335
HuuTri130401 <trith130401@gmail.com>
2023-07-20 22:01:26 +0700

    update Login.cshtml.cs

--- HistoryHub/Pages/Account/Login.cshtml.cs
+++ HistoryHub/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using BusinessObjects.Models;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Repositories;
